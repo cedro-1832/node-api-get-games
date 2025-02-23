565695a8db5092ef275cf4e8318b82c34ace0c3f
iancampusano <34844163+iancampusano@users.noreply.github.com>
2025-02-23 14:42:59 -0300

    fix error 4

--- config/db.js
+++ config/db.js
@@ -1,13 +1,18 @@
-const AWS = require('aws-sdk');
+const { DynamoDBClient } = require('@aws-sdk/client-dynamodb');
+const { DynamoDBDocumentClient } = require('@aws-sdk/lib-dynamodb');
 require('dotenv').config();
 
-AWS.config.update({
+// Configurar el cliente de DynamoDB con AWS SDK v3
+const client = new DynamoDBClient({
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  }
 });
 
-const dynamoDB = new AWS.DynamoDB.DocumentClient();
+// Usar DocumentClient para facilitar las operaciones
+const dynamoDB = DynamoDBDocumentClient.from(client);
 const TABLE_NAME = "PlayStationGames";
 
 module.exports = { dynamoDB, TABLE_NAME };
